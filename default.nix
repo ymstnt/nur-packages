8955518e30d3c99df8669cb6828a658e9a2052be
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-02-10 22:30:38 +0100

    example-package: remove

--- default.nix
+++ default.nix
@@ -14,7 +14,6 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
   beeper = pkgs.callPackage ./pkgs/beeper { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
