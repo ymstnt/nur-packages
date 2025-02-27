e8a2f923c7a007abcca5227cdb118852a93ed88c
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-02-27 12:17:07 +0100

    beeper: 4.0.470 -> 4.0.478

--- pkgs/beeper/default.nix
+++ pkgs/beeper/default.nix
@@ -10,10 +10,10 @@
 }:
 let
   pname = "beeper";
-  version = "4.0.470";
+  version = "4.0.478";
   src = fetchurl {
     url = "https://beeper-desktop.download.beeper.com/builds/Beeper-${version}.AppImage";
-    hash = "sha256-uHvXBz2y5kN8mU/cAaQbLgmsWtdPVUf70M4CLcYwsTg=";
+    hash = "sha256-SaYOYrSvzhYC8uPPEbBMTTZjOBSDxg4iU3qAf5ZaQTs=";
   };
 
   appimageContents = appimageTools.extract {
